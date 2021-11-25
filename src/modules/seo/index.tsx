027941a44b99ef6f7938a52cd37c2586235991a5
roy1210 <rswkure@gmail.com>
2021-11-25 18:55:33 +0800

    fix: update meta data

--- src/modules/seo/index.tsx
+++ src/modules/seo/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const SEO = () => {
   const content = 'Lightning Channel Explorer';
-  const title = 'DH Explorer';
+  const title = 'Lightning Channel Explorer';
   const image = 'og.png';
 
   return (
