9b812ba08cd509944389bc1e470b43fac1c57b39
roy1210 <rswkure@gmail.com>
2021-12-07 21:25:26 +0800

    fix: added pagination

--- src/modules/scenes/Main/ChannelList/index.tsx
+++ src/modules/scenes/Main/ChannelList/index.tsx
@@ -1,14 +1,12 @@
 import Image from 'next/image';
-import React, { useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
-// import AutoSizer from 'react-virtualized-auto-sizer';
-// import { FixedSizeList as List } from 'react-window';
-// import InfiniteLoader from 'react-window-infinite-loader';
 import { StickyContainer, Sticky } from 'react-sticky';
 import { useDebounce } from 'use-debounce';
 import { ScaleLoader } from 'react-spinners';
 import { useMatchMedia } from 'comet-ui-kit';
 import { rem } from 'polished';
+import ReactPaginate from 'react-paginate';
 
 import triangle from '../../../../../public/icons/triangle-orange.svg';
 import { Search } from '../../../../components/Search';
@@ -28,11 +26,43 @@ import {
   RowLoader,
 } from './styled';
 
+function Items({ currentItems, unit, nodeOwner }) {
+  return (
+    <>
+      {currentItems &&
+        currentItems.map((channel: IChannel) => (
+          <Channel unit={unit} nodeOwner={nodeOwner} channel={channel} key={channel.channelId} />
+        ))}
+    </>
+  );
+}
+
 export const ChannelList = ({ unit, nodeOwner }: { unit: TUnit; nodeOwner: string }) => {
   const [search, setSearch] = useState('');
   const [value] = useDebounce(search, 1000);
   const { channels, isLoading, setIsLoading, setChannels } = useGetChannelData(value);
   const [monitorChannelId, setMonitorChannelId] = useState<string>(null);
+
+  const [currentItems, setCurrentItems] = useState(null);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const itemsPerPage = 5;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+    setCurrentItems(channels.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(channels.length / itemsPerPage));
+  }, [itemOffset, channels, itemsPerPage]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % channels.length;
+    setItemOffset(newOffset);
+  };
+
+  useEffect(() => {
+    setItemOffset(0);
+  }, [search]);
 
   const valueProps = useMemo(
     () => ({ monitorChannelId, setMonitorChannelId }),
@@ -83,14 +113,17 @@ export const ChannelList = ({ unit, nodeOwner }: { unit: TUnit; nodeOwner: strin
         {!channels && !isLoading && <TextWaiting>No data returned from backend API</TextWaiting>}
         {channels && (
           <RequestInvoiceProvider value={valueProps}>
-            {channels.map((channel: IChannel) => (
-              <Channel
-                unit={unit}
-                nodeOwner={nodeOwner}
-                channel={channel}
-                key={channel.channelId}
-              />
-            ))}
+            <Items currentItems={currentItems} unit={unit} nodeOwner={nodeOwner} />
+            <ReactPaginate
+              className="pagination"
+              breakLabel="..."
+              nextLabel="next >"
+              onPageChange={handlePageClick}
+              pageRangeDisplayed={5}
+              pageCount={pageCount}
+              previousLabel="< previous"
+              renderOnZeroPageCount={null}
+            />
           </RequestInvoiceProvider>
         )}
       </StickyContainer>
